71b89d4c7fa20dabdc5f970075e408f7d5b248b4
AdrianBurgosA <aaburgos1@espe.edu.ec>
2022-08-02 17:39:50 -0500

    Agregadas nuevas funciones

--- api/uris.js
+++ api/uris.js
@@ -20,15 +20,76 @@ connection.connect(function(error){
 })
 
 //uris
-router.get("/getData",(err,res) => {
-   var query = connection.query('select tema.titulo, imagen.link, tema.definicion, tema.reconocer, tema.proteger, video.link_video from tema, video, imagen where tema.id_imagen = imagen.id_imagen and tema.id_video = video.id_video',  function(error, result){
-         if(error){
-            throw err;
-         }else{
-            res.send(result);
-         }
+const convertToInt = (res) => {
+   var ints = []
+   for(var i = 0; i < res.length; i++){
+      ints.push(parseInt(res[i]))
+   }
+   return ints
+}
+
+const splitArray = (res) => {
+   var ids = convertToInt(res.split(","))
+   return ids
+}
+
+router.get("/getSintomas",(err,res) => {
+   const query = connection.query("SELECT sintomas FROM sintomas", function(error,result){
+      if(error){
+         throw err
+      }else{
+         res.send(result)
       }
-      )
+   })
+})
+
+router.get("/getProteger",(err,res) => {
+   const query = connection.query("SELECT proteger FROM proteger", function(error,result){
+      if(error){
+         throw err
+      }else{
+         res.send(result)
+      }
+   })
+})
+
+router.get("/getSintomas",(err,res) => {
+   const query = connection.query("SELECT id_sintoma, sintomas FROM sintomas", function(error,result){
+      if(error){
+         throw err
+      }else{
+         res.send(result)
+      }
+   })
+})
+
+router.get("/getData",(err,res) => {
+   var query_string = 'SELECT tipo_riesgo.riesgo, riesgo.definicion, riesgo,video, riesgo.imagen, riesgo.id_sintomas, riesgo.id_proteger from tipo_riesgo, riesgo where riesgo.id_tipo = tipo_riesgo.id_tipo'
+   var query = connection.query(query_string,  function(error, result){
+      var riesgos = []
+      if(error){
+         throw err;
+      }else{
+         for(var i = 0; i < result.length; i++){
+            var riesgo = {
+               name: '',
+               def: '',
+               video: '',
+               img: '',
+               sintomas: [],
+               proteger: [],
+            }
+            riesgo.def = result[i].definicion
+            riesgo.name = result[i].riesgo
+            riesgo.video = result[i].video
+            riesgo.img = result[i].imagen
+            riesgo.sintomas  = splitArray(result[i].id_sintomas)
+            riesgo.proteger = splitArray(result[i].id_proteger)
+            riesgos.push(riesgo)
+         }
+            res.send(riesgos)
+      }
+   })
 })
    
 module.exports = router
